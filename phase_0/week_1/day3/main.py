002199852302de73fa7012b9256b5455b2d608e6
Rio Armiga <rioarmiga@gmail.com>
2022-04-13 14:15:34 +0700

    day3

---
+++ phase_0/week_1/day3/main.py
@@ -0,0 +1,7 @@
+# tanda bintang atau * digunakan untuk import semua
+from package.fisika import *
+
+print(kecepatan(100,5))
+
+from package.math1 import *
+print(perkalian(20,4))
---
+++ phase_0/week_1/day3/package/fisika.py
@@ -0,0 +1,7 @@
+def volume (panjang,lebar,tinggi):
+    ''' ini adalah rumus volume'''
+    return panjang*lebar*tinggi
+
+def kecepatan (jarak,waktu):
+    ''' ini adalah rumus kecepatan'''
+    return jarak/waktu 
